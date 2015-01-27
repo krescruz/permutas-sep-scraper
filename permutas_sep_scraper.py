77033d0665aeed5ff311c53a946f5b33387520ba
Jorge Lalo <hdez.jeduardo@gmail.com>
2015-01-26 19:57:23 -0600

    Added the page parameter:

--- permutas_sep_scraper.py
+++ permutas_sep_scraper.py
@@ -1,15 +1,7 @@
 import requests
 import bs4
 
-# response = requests.get('http://pyvideo.org/category/50/pycon-us-2014')
-# soup = bs4.BeautifulSoup(response.text)
-# links = soup.select('div.video-summary-data a[href^=/video]')
 
-response = requests.get('http://www.tulibrodevisitas.com/libro.php?id=11014')
-soup = bs4.BeautifulSoup(response.text)
-
-
-posts = []
 class Post:
 	def __init__(self):
 		self.username = ''
@@ -20,61 +12,71 @@ class Post:
 		self.publicationDate = ''
 		self.message = ''
 
-counter = 0
-for trs in soup.find_all('tr'):
+def scrape(page):
+	"If not page specified, the function will scrape the last page, ordered by publication date in a descendent order"
 
-	if trs.has_attr('bgcolor') and (str(trs['bgcolor']) == '#CCCCCC' or str(trs['bgcolor']) == '#FFFFFF'):
-		
-		counter = counter + 1
-		print counter
+	base_url = 'http://www.tulibrodevisitas.com/libro.php?id=11014'
+	posts = []
 
-		post = Post()
+	if page > 0:
+		base_url = base_url + '&paginacion=' + str(page)
 
-		for span in trs.find_all('span'):
-			if span.has_attr('class') and str(span['class'] == 'Mensaje'):
+	response = requests.get(base_url)
+	soup = bs4.BeautifulSoup(response.text)
 
-				msg = span.find_all()
-				if len(msg) == 0:
-					post.message = span.text.strip().replace('\n', ' ').replace('\r', '')
+	for trs in soup.find_all('tr'):
 
-				for strong in span.find_all('strong'):
-										
-					img_genre = strong.find_all('img')
-					if len(img_genre) > 0:
+		if trs.has_attr('bgcolor') and (str(trs['bgcolor']) == '#CCCCCC' or str(trs['bgcolor']) == '#FFFFFF'):
 
-						# Get the genre of the post owner
-						if img_genre[0]['src'] == 'Images/libro_mujer.gif':
-							post.genre = 'f'
-						elif img_genre[0]['src'] == 'Images/libro_hombre.gif':
-							post.genre = 'm'
+			post = Post()
+
+			for span in trs.find_all('span'):
+				if span.has_attr('class') and str(span['class'] == 'Mensaje'):
+
+					msg = span.find_all()
+					if len(msg) == 0:
+						post.message = span.text.strip().replace('\n', ' ').replace('\r', '')
+
+					for strong in span.find_all('strong'):
+											
+						img_genre = strong.find_all('img')
+						if len(img_genre) > 0:
+
+							# Get the genre of the post owner
+							if img_genre[0]['src'] == 'Images/libro_mujer.gif':
+								post.genre = 'f'
+							elif img_genre[0]['src'] == 'Images/libro_hombre.gif':
+								post.genre = 'm'
+							else:
+								post.genre = 'u'
+
+							
+							for img in img_genre:
+
+								# Get the website
+								if img['src'] == 'Images/libro_web.gif':
+									post.website = img.parent['href']
+
+								# Get the email
+								if img['src'] == 'Images/libro_mail.gif':
+									post.email = img.parent['href'].replace('mailto:','').strip().lower()
+
+
+
+						# Get the title of the publication
+						if len(strong) != 1:
+							lines = strong.text.strip().split('-')
+							if len(lines) > 1:
+								post.username = lines[0].strip().upper()
+								post.stateToOrFrom = lines[1].strip().upper()
+							else:
+								post.username = strong.text.strip()
 						else:
-							post.genre = 'u'
+							post.publicationDate = strong.text
 
-						
-						for img in img_genre:
+			posts.append(post);
 
-							# Get the website
-							if img['src'] == 'Images/libro_web.gif':
-								post.website = img.parent['href']
+	for post in posts:
+		print 'USERNAME: ' + post.username + ', ' + 'STATETOORFROM: ' + post.stateToOrFrom + ', ' + 'GENRE: ' + post.genre + ', ' + 'WEBSITE: ' + post.website + ', ' + 'EMAIL: ' + post.email + ', ' + 'PUBLICATIONDATE: ' + post.publicationDate + ', ' + 'MESSAGE: ' + post.message
 
-							# Get the email
-							if img['src'] == 'Images/libro_mail.gif':
-								post.email = img.parent['href'].replace('mailto:','').strip().lower()
-
-
-
-					# Get the title of the publication
-					if len(strong) != 1:
-						lines = strong.text.strip().split('-')
-						if len(lines) > 1:
-							post.username = lines[0].strip().upper()
-							post.stateToOrFrom = lines[1].strip().upper()
-						else:
-							post.username = strong.text.strip()
-					else:
-						post.publicationDate = strong.text
-
-		posts.append(post);
-
-for post in posts:
-	print 'USERNAME: ' + post.username + ', ' + 'STATETOORFROM: ' + post.stateToOrFrom + ', ' + 'GENRE: ' + post.genre + ', ' + 'WEBSITE: ' + post.website + ', ' + 'EMAIL: ' + post.email + ', ' + 'PUBLICATIONDATE: ' + post.publicationDate + ', ' + 'MESSAGE: ' + post.message
+scrape(99)

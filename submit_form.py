9a7cc529f2621fe4868b6ceaac3abc0fbbc2a005
lalongooo <hdez.jeduardo@gmail.com>
2015-01-28 11:33:06 -0600

    Test user name and email configuration on a different git client

--- submit_form.py
+++ submit_form.py
@@ -1,6 +1,7 @@
 import requests
 import locale
 
+#This is a comment for testing purposes
 
 locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 base_url = 'http://www.tulibrodevisitas.com/mensaje.php?id=11014'
